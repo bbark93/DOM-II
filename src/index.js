9c3a6d2b4f73ba65f46a68bc9f06ddf198e6cba5
Brandon Barksdale <bbark93@gmail.com>
2023-06-02 13:42:57 -0400

    added load event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './less/index.less'
 
 // Your code goes here!
+console.log('hello world');
+
+// 1- load
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! ready to go`);
+
+    const heading = document.querySelector('h1');
+    heading.textContent = 'READY TO GO!'
+}
